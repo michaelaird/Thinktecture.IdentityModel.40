2d3221cec401d23b8568f23934e7ad5d6a50b79d
Michael Aird <michael.aird@gmail.com>
2012-07-03 15:35:24 -0400

    Porting Authorization code from Thinktecture.IdentityModel.45

---
+++ IdentityModel/Thinktecture.IdentityModel/Authorization/WebApi/HttpAuthorizationContext.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Copyright (c) Dominick Baier.  All rights reserved.
+ * see license.txt
+ */
+
+using System.Threading;
+//using System.Security.Claims;
+using System.Web.Http.Controllers;
+using Microsoft.IdentityModel.Claims;
+
+namespace Thinktecture.IdentityModel.Authorization.WebApi
+{
+    public class HttpAuthorizationContext : AuthorizationContext
+    {
+        private HttpActionContext _context;
+
+        protected HttpActionContext ActionContext
+        {
+            get { return _context; }
+        }
+
+        public HttpAuthorizationContext(HttpActionContext context)
+            : base((ClaimsPrincipal)Thread.CurrentPrincipal,
+                   context.ControllerContext.ControllerDescriptor.ControllerName,
+                   context.Request.Method.Method)
+        {
+            _context = context;
+        }
+    }
+}
